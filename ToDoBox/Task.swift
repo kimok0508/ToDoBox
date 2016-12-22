05621031b8c5f495a50b5fad459ab950e451c11f
MacBookPro <MacBookPro@MacBookProui-MacBook-Pro.local>
2016-12-22 21:40:07 +0900

    task remove and task rearrange feature added

--- ToDoBox/Task.swift
+++ ToDoBox/Task.swift
@@ -11,9 +11,22 @@ struct Task{
     var memo : String?
     var done : Bool
     
-    init(title : String, memo : String? = nil){
+    init(title : String, memo : String? = nil, done : Bool = false){
         self.title = title
         self.memo = memo
-        self.done = false
+        self.done = done
+    }
+    
+    init?(dictionary : [String : Any]){
+        guard
+            let title = dictionary["title"] as? String,
+            let done = dictionary["done"] as? Bool
+        else{
+            return nil
+        }
+        
+        self.title = title
+        self.memo = dictionary["memo"] as? String
+        self.done = done
     }
 }
--- ToDoBox/TaskListViewController.swift
+++ ToDoBox/TaskListViewController.swift
@@ -10,31 +10,49 @@ import UIKit
 
 class TaskListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet var tableView : UITableView!
+    let doneButton : UIBarButtonItem = UIBarButtonItem(
+        barButtonSystemItem : .done,
+        target : nil,
+        action : #selector(doneButtonDidTap)
+    )
+    @IBOutlet var editButton : UIBarButtonItem!
     
-    var tasks : [Task] = []
-
+    var tasks : [Task] = []{
+        didSet{
+            self.saveTasks()
+        }
+    }
+    
+    @IBAction func editButtonDidTap(){
+        self.navigationItem.leftBarButtonItem = self.doneButton
+        self.doneButton.target = self
+        self.tableView.setEditing(true, animated: true)
+    }
+    
+    func doneButtonDidTap(){
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        self.editButton.target = self
+        self.tableView.setEditing(false, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "ToDo List"
         loadTasks()
-        self.tableView.reloadData()
     }
     
     func loadTasks(){
         guard
             let savedDictionaries = UserDefaults.standard.array(forKey : "tasks") as? [[String : Any?]]
-        else{
+            else{
                 return
         }
         
-        self.tasks = savedDictionaries.map{ task -> Task in
-            return Task(
-                title : task["title"] as! String,
-                memo : task["memo"] as! String?,
-                done : task["done"] as! Bool
-            )
+        self.tasks = savedDictionaries.flatMap{ dict -> Task? in
+            return Task(dictionary: dict)
         }
+        self.tableView.reloadData()
     }
     
     func saveTasks(){
@@ -56,20 +74,20 @@ class TaskListViewController: UIViewController, UITableViewDataSource, UITableVi
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        saveTasks()
     }
     
     override func prepare(for segue : UIStoryboardSegue, sender : Any?){
         if let navigationController = segue.destination as? UINavigationController,
             let taskEditViewController = navigationController.viewControllers.first as?TaskEditViewController{
-                taskEditViewController.taskDidAdd = {
-                    newTask in
-                    self.tasks.append(newTask)
-                    self.saveTasks()
-                    self.tableView.reloadData()
+            taskEditViewController.taskDidAdd = {
+                newTask in
+                self.tasks.append(newTask)
+                self.tableView.reloadData()
             }
         }
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
     }
@@ -88,6 +106,7 @@ class TaskListViewController: UIViewController, UITableViewDataSource, UITableVi
         return cell
     }
     
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let i = indexPath.row
         var task = tasks[i]
@@ -95,6 +114,22 @@ class TaskListViewController: UIViewController, UITableViewDataSource, UITableVi
         tasks[i] = task
         
         tableView.reloadRows(at: [indexPath], with: .fade)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        self.tasks.remove(at: indexPath.row)
+        self.tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var newTasks = self.tasks
+        let task = self.tasks.remove(at : sourceIndexPath.row)
+        newTasks.insert(task, at: destinationIndexPath.row)
+        self.tasks = newTasks
     }
 }
 
